516748807a97a3cf09ca07dd7f2eaf12e06ab7ae
Ubuntu <ubuntu@ip-172-31-82-78.ec2.internal>
2019-10-31 13:41:01 +0000

    Updated file

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+if os.path.exists('env.py'):
+    import env
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i380y@%0)v$bun2fpbsfk^0rpspa8i-$3bp$bgb*7%htpzhkgm'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
