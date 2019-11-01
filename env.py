bfd2ee2f0ef840a7468e3fbe90cd094ef381d6ac
Paul Friel <44071858+Spagettileg@users.noreply.github.com>
2019-11-01 09:31:37 +0000

    Delete env.py
    File moved to .gitignore file

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "'bjgwaof!dmgopu5q%%8xmplywp168dybv17e4iir0707wwpjgl'")
-
-os.environ.setdefault("DATABASE_URL", "postgres://jfsqjzdjfvkhqo:c91a951560a767ab3b7c8aa61bf5dbdd2cbd3fd81efba04cbcabd4e83e852eb9@ec2-46-137-159-254.eu-west-1.compute.amazonaws.com:5432/d4ic5j5atjbnup")
